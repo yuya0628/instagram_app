1579813e17b108b2a3127f4d9864764324717838
yuya0628 <sonoyuki13@gmail.com>
2021-10-11 09:15:10 +0900

    ユーザー詳細

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name,:email, :password,:avatar)
+    params.require(:user).permit(:name, :email, :password)
   end
 end
--- app/forms/search_posts_form.rb
+++ app/forms/search_posts_form.rb
@@ -9,9 +9,9 @@ class SearchPostsForm
 
  def search
   scope = Post.distinct
-  scope = splited_bodies.map{|splited_body| scope.body_contain(splited_body)}.inject{|result, scp| result.or(scp)} if body.present?
-  scope = scope.comment_body_contain(comment_body) if comment_body.present?
-  scope = scope.username_contain(username) if username.present?
+  scope = splited_bodies.map{|splited_body| scope.body_contain(splited_body)}.inject{|result, scp| result.or(scp)} if self.body.present?
+  scope = scope.comment_body_contain(comment_body) if self.comment_body.present?
+  scope = scope.username_contain(username) if self.username.present?
   scope
  end 
 
